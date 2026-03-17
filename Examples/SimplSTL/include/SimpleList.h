2d9bf61bee980844bde54d0dc7e899e014575358
zhanghongwei <zhanghongwei@yjk.cn>
2026-03-17 09:49:49 +0800

    修改编译错误 添加Test

---
+++ Examples/SimplSTL/include/SimpleList.h
@@ -0,0 +1,253 @@
+﻿#pragma once
+
+template <typename>
+class SimpleList;
+
+template <typename T>
+class SimpleListIterator;
+
+template <typename T>
+class SimpleListNode
+{
+public:
+    SimpleListNode(const T& value)
+        : m_value(value), m_pNext(nullptr), m_pPrev(nullptr)
+    {
+    }
+
+private:
+    T m_value;
+    SimpleListNode* m_pNext;
+    SimpleListNode* m_pPrev;
+
+    template <typename>
+    friend class SimpleList;
+
+    template <typename>
+    friend class SimpleListIterator;
+};
+
+template <typename T>
+class SimpleListIterator
+{
+public:
+    using Node = SimpleListNode<T>;
+    SimpleListIterator(Node* node = nullptr) : m_pNode(node)
+    {
+    }
+
+    T& operator*()
+    {
+        return m_pNode->m_value;
+    }
+    const T& operator*() const
+    {
+        return m_pNode->m_value;
+    }
+
+    SimpleListIterator& operator++()
+    {
+        m_pNode = m_pNode->m_pNext;
+        return *this;
+    }
+    SimpleListIterator& operator--()
+    {
+        m_pNode = m_pNode->m_pPrev;
+        return *this;
+    }
+    bool operator!=(const SimpleListIterator& other) const
+    {
+        return m_pNode != other.m_pNode;
+    }
+
+    bool operator==(const SimpleListIterator& other) const
+    {
+        return m_pNode == other.m_pNode;
+    }
+
+private:
+    Node* m_pNode;
+    template <typename>
+    friend class SimpleList;
+};
+
+template <typename T>
+class SimpleList
+{
+public:
+    using Iterator = SimpleListIterator<T>;
+    using Node = SimpleListNode<T>;
+
+public:
+    SimpleList();
+    ~SimpleList();
+
+    void PushBack(const T& value);
+    void PushFront(const T& value);
+
+    SimpleListIterator<T> Insert(Iterator pos, const T& value);
+    SimpleListIterator<T> Erase(Iterator pos);
+
+    void PopBack();
+    void PopFront();
+
+    size_t Size() const;
+    void Clear();
+
+    Iterator Begin() const
+    {
+        return Iterator(m_pSentinel->m_pNext);
+    }
+
+    Iterator End() const
+    {
+        return Iterator(m_pSentinel);
+    }
+    /*把 other 中 it 指向的节点
+    移动到当前链表 pos 位置之前*/
+    void Splice(SimpleListIterator<T> pos, SimpleList& other,
+                SimpleListIterator<T> it);
+
+private:
+    // SimpleListNode<T>* m_pHead;
+    // SimpleListNode<T>* m_pTail;
+    Node* m_pSentinel;
+    size_t m_size;
+
+private:
+    // 禁止拷贝构造和赋值操作
+    SimpleList(const SimpleList&) = delete;
+    SimpleList& operator=(const SimpleList&) = delete;
+};
+
+template <typename T>
+inline SimpleList<T>::SimpleList() : m_pSentinel(nullptr), m_size(0)
+{
+    m_pSentinel = new Node(T());         // 创建哨兵节点
+    m_pSentinel->m_pNext = m_pSentinel;  // 哨兵节点的下一个指向自己
+    m_pSentinel->m_pPrev = m_pSentinel;  // 哨兵节点的上一个指向自己
+}
+
+template <typename T>
+inline SimpleList<T>::~SimpleList()
+{
+    Clear();
+    delete m_pSentinel;
+}
+
+template <typename T>
+inline void SimpleList<T>::PushBack(const T& value)
+{
+    Insert(End(), value);
+}
+
+template <typename T>
+inline void SimpleList<T>::PushFront(const T& value)
+{
+    Insert(Begin(), value);
+}
+
+template <typename T>
+inline SimpleListIterator<T> SimpleList<T>::Insert(Iterator pos, const T& value)
+{
+    auto* pNode = pos.m_pNode;
+    auto* pNewNode = new SimpleListNode<T>(value);
+
+    Node* pPrev = pNode->m_pPrev;
+    pPrev->m_pNext = pNewNode;
+    pNewNode->m_pPrev = pPrev;
+
+    pNewNode->m_pNext = pNode;
+    pNode->m_pPrev = pNewNode;
+
+    ++m_size;
+    return Iterator(pNewNode);
+}
+
+template <typename T>
+inline SimpleListIterator<T> SimpleList<T>::Erase(Iterator pos)
+{
+    auto* pNode = pos.m_pNode;
+    if (pNode == m_pSentinel) {
+        return End();  // 不允许删除哨兵节点，直接返回原位置
+    }
+    auto* pPrev = pNode->m_pPrev;
+    auto* pNext = pNode->m_pNext;
+
+    pPrev->m_pNext = pNext;
+    pNext->m_pPrev = pPrev;
+
+    delete pNode;
+    --m_size;
+    return Iterator(pNext);
+}
+
+template <typename T>
+inline size_t SimpleList<T>::Size() const
+{
+    return m_size;
+}
+
+template <typename T>
+inline void SimpleList<T>::Clear()
+{
+    SimpleListNode<T>* current = m_pSentinel->m_pNext;
+    while (current != m_pSentinel) {
+        SimpleListNode<T>* next = current->m_pNext;
+        delete current;
+        current = next;
+    }
+    m_pSentinel->m_pNext = m_pSentinel;
+    m_pSentinel->m_pPrev = m_pSentinel;
+    m_size = 0;
+}
+
+template <typename T>
+inline void SimpleList<T>::Splice(SimpleListIterator<T> pos, SimpleList& other,
+                                  SimpleListIterator<T> it)
+{
+    auto* pOtherNode = it.m_pNode;
+    if (pOtherNode == other.m_pSentinel) {
+        return;  // it 指向的是 other 的哨兵节点，不进行操作
+    }
+
+    if (pos == it)
+        return;  // pos 和 it 指向同一个位置，不进行操作
+    if (this == &other && pos.m_pNode == it.m_pNode->m_pNext)
+        return;  
+
+    // 从 other 中移除 pOtherNode
+    auto* pOtherPrev = pOtherNode->m_pPrev;
+    auto* pOtherNext = pOtherNode->m_pNext;
+    pOtherNext->m_pPrev = pOtherPrev;
+    pOtherPrev->m_pNext = pOtherNext;
+
+    // 将 pOtherNode 插入到当前链表 pos 位置之前
+    auto* pPosNode = pos.m_pNode;
+    auto* pPosPrev = pPosNode->m_pPrev;
+    pPosPrev->m_pNext = pOtherNode;
+    pOtherNode->m_pPrev = pPosPrev;
+    pOtherNode->m_pNext = pPosNode;
+    pPosNode->m_pPrev = pOtherNode;
+
+    if (this != &other) {
+        --other.m_size;
+        ++m_size;
+    }
+}
+
+template <typename T>
+inline void SimpleList<T>::PopFront()
+{
+    if (m_size == 0)
+        return;  // 链表为空，不进行操作
+    Erase(Begin());
+}
+
+template <typename T>
+inline void SimpleList<T>::PopBack()
+{
+    if (m_size == 0)
+        return;  // 链表为空，不进行操作
+    Erase(--End());
+}
---
+++ Examples/Test/SimpleListTest.cpp
@@ -0,0 +1,378 @@
+﻿#include "pch.h"
+#include "gtest/gtest.h"
+#include "SimpleList.h"
+#include <vector>
+
+class SimpleListTest : public testing::Test
+{
+protected:
+    void SetUp() override
+    {
+        // 按需初始化
+    }
+
+    void TearDown() override
+    {
+    }
+
+    std::vector<int> ToVector(const SimpleList<int>& list)
+    {
+        std::vector<int> values;
+        for (auto it = list.Begin(); it != list.End(); ++it) {
+            values.push_back(*it);
+        }
+        return values;
+    }
+
+protected:
+    SimpleList<int> m_list;
+};
+
+TEST_F(SimpleListTest, DefaultConstructedListIsEmpty)
+{
+    EXPECT_EQ(m_list.Size(), 0u);
+    EXPECT_EQ(m_list.Begin(), m_list.End());
+}
+
+TEST_F(SimpleListTest, PushBackWorks)
+{
+    m_list.PushBack(1);
+    m_list.PushBack(2);
+    m_list.PushBack(3);
+
+    EXPECT_EQ(m_list.Size(), 3u);
+
+    std::vector<int> expected{1, 2, 3};
+    EXPECT_EQ(ToVector(m_list), expected);
+}
+
+TEST_F(SimpleListTest, PushFrontWorks)
+{
+    m_list.PushFront(3);
+    m_list.PushFront(2);
+    m_list.PushFront(1);
+
+    EXPECT_EQ(m_list.Size(), 3u);
+
+    std::vector<int> expected{1, 2, 3};
+    EXPECT_EQ(ToVector(m_list), expected);
+}
+
+TEST_F(SimpleListTest, PushFrontAndPushBackMixed)
+{
+    m_list.PushBack(2);
+    m_list.PushFront(1);
+    m_list.PushBack(3);
+
+    EXPECT_EQ(m_list.Size(), 3u);
+
+    std::vector<int> expected{1, 2, 3};
+    EXPECT_EQ(ToVector(m_list), expected);
+}
+
+TEST_F(SimpleListTest, InsertBeforeBegin)
+{
+    m_list.PushBack(2);
+    m_list.PushBack(3);
+
+    m_list.Insert(m_list.Begin(), 1);
+
+    EXPECT_EQ(m_list.Size(), 3u);
+
+    std::vector<int> expected{1, 2, 3};
+    EXPECT_EQ(ToVector(m_list), expected);
+}
+
+TEST_F(SimpleListTest, InsertBeforeMiddlePosition)
+{
+    m_list.PushBack(1);
+    m_list.PushBack(3);
+
+    auto it = m_list.Begin();
+    ++it;  // 指向 3
+
+    m_list.Insert(it, 2);
+
+    EXPECT_EQ(m_list.Size(), 3u);
+
+    std::vector<int> expected{1, 2, 3};
+    EXPECT_EQ(ToVector(m_list), expected);
+}
+
+TEST_F(SimpleListTest, InsertBeforeEndEqualsPushBack)
+{
+    m_list.PushBack(1);
+    m_list.PushBack(2);
+
+    m_list.Insert(m_list.End(), 3);
+
+    EXPECT_EQ(m_list.Size(), 3u);
+
+    std::vector<int> expected{1, 2, 3};
+    EXPECT_EQ(ToVector(m_list), expected);
+}
+
+TEST_F(SimpleListTest, EraseBegin)
+{
+    m_list.PushBack(1);
+    m_list.PushBack(2);
+    m_list.PushBack(3);
+
+    m_list.Erase(m_list.Begin());
+
+    EXPECT_EQ(m_list.Size(), 2u);
+
+    std::vector<int> expected{2, 3};
+    EXPECT_EQ(ToVector(m_list), expected);
+}
+
+TEST_F(SimpleListTest, EraseMiddle)
+{
+    m_list.PushBack(1);
+    m_list.PushBack(2);
+    m_list.PushBack(3);
+
+    auto it = m_list.Begin();
+    ++it;  // 指向 2
+
+    m_list.Erase(it);
+
+    EXPECT_EQ(m_list.Size(), 2u);
+
+    std::vector<int> expected{1, 3};
+    EXPECT_EQ(ToVector(m_list), expected);
+}
+
+TEST_F(SimpleListTest, EraseEndDoesNothing)
+{
+    m_list.PushBack(1);
+    m_list.PushBack(2);
+
+    m_list.Erase(m_list.End());
+
+    EXPECT_EQ(m_list.Size(), 2u);
+
+    std::vector<int> expected{1, 2};
+    EXPECT_EQ(ToVector(m_list), expected);
+}
+
+TEST_F(SimpleListTest, PopFrontOnEmptyListDoesNothing)
+{
+    m_list.PopFront();
+
+    EXPECT_EQ(m_list.Size(), 0u);
+    EXPECT_EQ(m_list.Begin(), m_list.End());
+}
+
+TEST_F(SimpleListTest, PopBackOnEmptyListDoesNothing)
+{
+    m_list.PopBack();
+
+    EXPECT_EQ(m_list.Size(), 0u);
+    EXPECT_EQ(m_list.Begin(), m_list.End());
+}
+
+TEST_F(SimpleListTest, PopFrontWorks)
+{
+    m_list.PushBack(1);
+    m_list.PushBack(2);
+    m_list.PushBack(3);
+
+    m_list.PopFront();
+
+    EXPECT_EQ(m_list.Size(), 2u);
+
+    std::vector<int> expected{2, 3};
+    EXPECT_EQ(ToVector(m_list), expected);
+}
+
+TEST_F(SimpleListTest, PopBackWorks)
+{
+    m_list.PushBack(1);
+    m_list.PushBack(2);
+    m_list.PushBack(3);
+
+    m_list.PopBack();
+
+    EXPECT_EQ(m_list.Size(), 2u);
+
+    std::vector<int> expected{1, 2};
+    EXPECT_EQ(ToVector(m_list), expected);
+}
+
+TEST_F(SimpleListTest, PopFrontOnSingleElementList)
+{
+    m_list.PushBack(1);
+
+    m_list.PopFront();
+
+    EXPECT_EQ(m_list.Size(), 0u);
+    EXPECT_EQ(m_list.Begin(), m_list.End());
+}
+
+TEST_F(SimpleListTest, PopBackOnSingleElementList)
+{
+    m_list.PushBack(1);
+
+    m_list.PopBack();
+
+    EXPECT_EQ(m_list.Size(), 0u);
+    EXPECT_EQ(m_list.Begin(), m_list.End());
+}
+
+TEST_F(SimpleListTest, ClearWorks)
+{
+    m_list.PushBack(1);
+    m_list.PushBack(2);
+    m_list.PushBack(3);
+
+    m_list.Clear();
+
+    EXPECT_EQ(m_list.Size(), 0u);
+    EXPECT_EQ(m_list.Begin(), m_list.End());
+}
+
+TEST_F(SimpleListTest, IteratorTraversalWorks)
+{
+    m_list.PushBack(10);
+    m_list.PushBack(20);
+    m_list.PushBack(30);
+
+    auto it = m_list.Begin();
+    ASSERT_NE(it, m_list.End());
+    EXPECT_EQ(*it, 10);
+
+    ++it;
+    ASSERT_NE(it, m_list.End());
+    EXPECT_EQ(*it, 20);
+
+    ++it;
+    ASSERT_NE(it, m_list.End());
+    EXPECT_EQ(*it, 30);
+
+    ++it;
+    EXPECT_EQ(it, m_list.End());
+}
+
+TEST_F(SimpleListTest, SpliceSingleNodeFromOtherList)
+{
+    m_list.PushBack(1);
+    m_list.PushBack(3);
+
+    SimpleList<int> other;
+    other.PushBack(10);
+    other.PushBack(20);
+    other.PushBack(30);
+
+    auto pos = m_list.Begin();
+    ++pos;  // 指向 3
+
+    auto it = other.Begin();
+    ++it;  // 指向 20
+
+    m_list.Splice(pos, other, it);
+
+    EXPECT_EQ(m_list.Size(), 3u);
+    EXPECT_EQ(other.Size(), 2u);
+
+    std::vector<int> expectedThis{1, 20, 3};
+    std::vector<int> expectedOther{10, 30};
+
+    EXPECT_EQ(ToVector(m_list), expectedThis);
+    EXPECT_EQ(ToVector(other), expectedOther);
+}
+
+TEST_F(SimpleListTest, SpliceNodeToEndFromOtherList)
+{
+    m_list.PushBack(1);
+    m_list.PushBack(2);
+
+    SimpleList<int> other;
+    other.PushBack(3);
+
+    m_list.Splice(m_list.End(), other, other.Begin());
+
+    EXPECT_EQ(m_list.Size(), 3u);
+    EXPECT_EQ(other.Size(), 0u);
+
+    std::vector<int> expectedThis{1, 2, 3};
+    EXPECT_EQ(ToVector(m_list), expectedThis);
+    EXPECT_EQ(other.Begin(), other.End());
+}
+
+TEST_F(SimpleListTest, SpliceEndIteratorFromOtherDoesNothing)
+{
+    m_list.PushBack(1);
+    m_list.PushBack(2);
+
+    SimpleList<int> other;
+    other.PushBack(3);
+    other.PushBack(4);
+
+    m_list.Splice(m_list.Begin(), other, other.End());
+
+    EXPECT_EQ(m_list.Size(), 2u);
+    EXPECT_EQ(other.Size(), 2u);
+
+    std::vector<int> expectedThis{1, 2};
+    std::vector<int> expectedOther{3, 4};
+
+    EXPECT_EQ(ToVector(m_list), expectedThis);
+    EXPECT_EQ(ToVector(other), expectedOther);
+}
+
+TEST_F(SimpleListTest, SpliceSameListPosEqualsItDoesNothing)
+{
+    m_list.PushBack(1);
+    m_list.PushBack(2);
+    m_list.PushBack(3);
+
+    auto it = m_list.Begin();
+    ++it;  // 指向 2
+
+    m_list.Splice(it, m_list, it);
+
+    EXPECT_EQ(m_list.Size(), 3u);
+
+    std::vector<int> expected{1, 2, 3};
+    EXPECT_EQ(ToVector(m_list), expected);
+}
+
+TEST_F(SimpleListTest, SpliceSameListMoveNodeForward)
+{
+    m_list.PushBack(1);
+    m_list.PushBack(2);
+    m_list.PushBack(3);
+    m_list.PushBack(4);
+
+    auto it = m_list.Begin();
+    ++it;  // 指向 2
+
+    auto pos = m_list.End();  // 移到末尾前，相当于放到最后
+    m_list.Splice(pos, m_list, it);
+
+    EXPECT_EQ(m_list.Size(), 4u);
+
+    std::vector<int> expected{1, 3, 4, 2};
+    EXPECT_EQ(ToVector(m_list), expected);
+}
+
+TEST_F(SimpleListTest, SpliceSameListAdjacentNoChange)
+{
+    m_list.PushBack(1);
+    m_list.PushBack(2);
+    m_list.PushBack(3);
+
+    auto it = m_list.Begin();
+    ++it;  // 指向 2
+
+    auto pos = it;
+    ++pos;  // 指向 3，2 本来就在 3 前面
+
+    m_list.Splice(pos, m_list, it);
+
+    EXPECT_EQ(m_list.Size(), 3u);
+
+    std::vector<int> expected{1, 2, 3};
+    EXPECT_EQ(ToVector(m_list), expected);
+}
